8b094dfce904c9bfec226d25e95421947f0cb37a
Aduet Dabral <98040707+AduetDabral1@users.noreply.github.com>
2022-05-30 00:15:38 +0530

    Add files via upload

--- myApp/src/components/Navbar.js
+++ myApp/src/components/Navbar.js
@@ -25,7 +25,7 @@ export default function Navbar(props) {
           <button className="btn btn-primary" type="submit">Search</button>
         </form>
         <div className={`form-check form-switch text-${props.mode==="light"?"dark":"light"}`}>
-  <input className="form-check-input mx-3" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+  <input className="form-check-input mx-3" onClick={props.toggleMode} type="checkbox" role="switch" aria-checked id="flexSwitchCheckDefault"/>
   <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
 </div>
       </div>
--- myApp/src/components/TextForm.js
+++ myApp/src/components/TextForm.js
@@ -1,6 +1,7 @@
 import React,{useState} from 'react'
 
 export default function TextForm(props) {
+
     const [text,setText] = useState("");
     const handleUpClick = () => {
         console.log("Uppercase was clicked " + text);
@@ -28,14 +29,12 @@ export default function TextForm(props) {
     }
 const handleCopy = () => {
     console.log ("Copying text");
-    let text = document.getElementById("textbox");
-    text.select();
-    navigator.clipboard.writeText(text.value);
+    navigator.clipboard.writeText(text);
     props.showAlert("Copied to clipboard", "success");
 }
 
 const handleExtraSpaces =() => {
-    let newText = text.split(/[  ]+/);
+    let newText = text.split(/[ ]+/);
     setText(newText.join(" "));
     props.showAlert("Extra spaces removed", "success");
 }
@@ -46,18 +45,18 @@ const handleExtraSpaces =() => {
             <h1 style={{ color : props.mode==="light"?"black":"white"}}>{props.heading}</h1>
             <textarea className="form-control" id="textbox" value={text} style={{ backgroundColor : props.mode==="light"?"white":"#D0CFFF",  color : props.mode==="light"?"black":"#4F0171"}} rows="8" onChange={handleOnChange}></textarea>
             </div>
-            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to uppercase</button>
-            <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to lowercase</button>
-            <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
-            <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
-            <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+            <button disabled= {text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert to uppercase</button>
+            <button disabled= {text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleLowClick}>Convert to lowercase</button>
+            <button disabled= {text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleClearClick}>Clear Text</button>
+            <button disabled= {text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleCopy}>Copy Text</button>
+            <button disabled= {text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
     </div>
 
     <div className="container my-3"  style={{ color : props.mode==="light"?"black":"white"}}>
             <h2>Text Summary</h2>
-            <p>Number of words : {text.split(" ").length}</p>
+            <p>Number of words : {text.split(/\s+/).filter((element)=>{return element.length!==0}).length}</p>
             <p>Number of characters : {text.length}</p>
-            <p>{0.008 * text.split(" ").length} minutes read</p>
+            <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} minutes read</p>
 
             <h2>Preview</h2>
             <p>{text.length>0?text:"Enter text to preview it"}</p>
