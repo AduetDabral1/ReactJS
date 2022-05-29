26fc0ada849a3e5f45735d71191e0d72f5ef70a9
Aduet Dabral <98040707+AduetDabral1@users.noreply.github.com>
2022-05-30 00:20:39 +0530

    Add files via upload

---
+++ myApp/build/precache-manifest.06c02cdedcf0aab7ce7e2617a6392cbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d923649ac4787af7923b",
+    "url": "/static/css/main.2f8e9d0c.chunk.css"
+  },
+  {
+    "revision": "d923649ac4787af7923b",
+    "url": "/static/js/main.d923649a.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "8adafcab8afea751c5fb",
+    "url": "/static/js/2.8adafcab.chunk.js"
+  },
+  {
+    "revision": "769c94daf783692f2c65",
+    "url": "/static/js/runtime~main.769c94da.js"
+  },
+  {
+    "revision": "085709d7b188eb0a306eac2d898ff05d",
+    "url": "/index.html"
+  }
+];
